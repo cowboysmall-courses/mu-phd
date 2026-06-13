aace45bdb3dc1561bbb8a6198b72e8eb75a22f91
Jerry Kiely <jerry@cowboysmall.com>
2026-06-13 20:16:56 +0100

    added some experiments

---
+++ src/sdc/experiment_sdc_add.py
@@ -0,0 +1,54 @@
+
+from typing import List
+
+from sdc import Tile
+
+def get_add_tiles(bits1: str, bits2: str) -> List[Tile]:
+    chunks = [f"{b1}{b2}" for b1, b2 in zip(reversed(bits1), reversed(bits2))]
+
+    z = sum([int(b) for b in chunks[0]])
+    tiles = [Tile(0, z // 2, chunks[0], chunks[1], z % 2)]
+
+    for i in range(1, len(chunks) - 1):
+        z = sum([int(b) for b in chunks[i]])
+        tiles.append(Tile(tiles[-1].right_instruction, (tiles[-1].right_instruction + z) // 2, chunks[i], chunks[i + 1], (tiles[-1].right_instruction + z) % 2))
+
+    z = sum([int(b) for b in chunks[-1]])
+    tiles.append(Tile(tiles[-1].right_instruction, tiles[-1].right_instruction, chunks[-1], "--", (tiles[-1].right_instruction + z) % 2))
+
+    return tiles
+
+
+def main() -> None:
+    print("SDC: Add\n")
+
+    bits1 = "1010"
+    bits2 = "0011"
+    tiles = get_add_tiles(bits1, bits2)
+    print(f"{bits1} + {bits2} = {''.join(reversed([str(tile.value) for tile in tiles]))}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits1 = "0111"
+    bits2 = "0001"
+    tiles = get_add_tiles(bits1, bits2)
+    print(f"{bits1} + {bits2} = {''.join(reversed([str(tile.value) for tile in tiles]))}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits1 = "1100"
+    bits2 = "0011"
+    tiles = get_add_tiles(bits1, bits2)
+    print(f"{bits1} + {bits2} = {''.join(reversed([str(tile.value) for tile in tiles]))}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits1 = "1111"
+    bits2 = "0001"
+    tiles = get_add_tiles(bits1, bits2)
+    print(f"{bits1} + {bits2} = {''.join(reversed([str(tile.value) for tile in tiles]))}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
---
+++ src/sdc/experiment_sdc_parity.py
@@ -0,0 +1,49 @@
+
+from typing import List
+
+from sdc import Tile
+
+PMAP = {"00": 0, "01": 1, "10": 1, "11": 0}
+
+def get_parity_tiles(bits: str) -> List[Tile]:
+    chunks = [bits[i:i + 2] for i in range(0, len(bits), 2)]
+
+    tiles  = [Tile(0, PMAP[chunks[0]], chunks[0], chunks[1], PMAP[chunks[0]])]
+    for i in range(1, len(chunks) - 1):
+        b1 = chunks[i]
+        b2 = chunks[i + 1]
+        tiles.append(Tile(tiles[-1].right_instruction, (tiles[-1].right_instruction + PMAP[b1]) % 2, b1, b2, (tiles[-1].value + PMAP[b1]) % 2))
+    tiles.append(Tile(tiles[-1].right_instruction, (tiles[-1].right_instruction + PMAP[chunks[-1]]) % 2, chunks[-1], "--", (tiles[-1].value + PMAP[chunks[-1]]) % 2))
+
+    return tiles
+
+
+def main() -> None:
+    print("SDC: Parity\n")
+
+    bits  = "10100100"
+    tiles = get_parity_tiles(bits)
+    print(f"Parity({bits}) = {tiles[-1].value}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits  = "00100100"
+    tiles = get_parity_tiles(bits)
+    print(f"Parity({bits}) = {tiles[-1].value}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits  = "11111111"
+    tiles = get_parity_tiles(bits)
+    print(f"Parity({bits}) = {tiles[-1].value}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
+
+
+    bits  = "00000000"
+    tiles = get_parity_tiles(bits)
+    print(f"Parity({bits}) = {tiles[-1].value}")
+    print("\n".join(str(tile) for tile in tiles))
+    print()
