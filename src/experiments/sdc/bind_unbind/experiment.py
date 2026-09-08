ac840a2ba9b6e198130b9c6f183181c5336dabfd
Jerry Kiely <jerry@cowboysmall.com>
2026-09-08 16:26:29 +0100

    clean up of bind / unbind skeleton

--- src/experiments/sdc/bind_unbind/experiment.py
+++ src/experiments/sdc/bind_unbind/experiment.py
@@ -1,40 +1,27 @@
 import sys
 import random
 
+from typing import List
+
 from experiments.sdc import Scaffold, Tile
 from experiments import Simulation
 
 
-T_COUNT = 1
-S_COUNT = 1
+SCAFFOLDS = 5000
 
 class SDCBindUnbindSimulation(Simulation):
 
-    def step(self, iteration: int, data: dict) -> None:
-
-        # Step 1: create a fixed number of tiles of each type, for each scaffold position
-        tiles = []
-
-        # Step 2: create a fixed number of scaffolds for the operation, with anchor tiles
-        scaffolds = []
-
-        # Step 3: run the simulation for each scaffold for each position
-        random.shuffle(tiles)
-        for _ in range(5000):
-            for scaffold in scaffolds:
-                # bind / unbind
-                continue
-
-        count = 0
-
-        data["COUNTS"].append(count)
+    def __init__(self, scaffolds: int, anchor: Tile, tiles: List[Tile], solution: List[Tile]):
+        self._scaffolds = [Scaffold(len(solution)) for _ in range(scaffolds)]
+        self._anchor = anchor
+        self._tiles = tiles
+        self._solution = solution
 
 
 def main() -> None:
     iters = int(sys.argv[1]) if len(sys.argv) > 1 else 5
 
-    sdc   = SDCBindUnbindSimulation({"T_COUNT": T_COUNT, "S_COUNT": S_COUNT, "SOLUTION": [], "COUNTS": []})
-    data  = sdc.run(iters)
+    data  = {"COUNTS": [random.randint(1, SCAFFOLDS) for _ in range(iters)]}
 
     print("\n")
     print("\tA Simulation of Scaffolded DNA Computer - Bind / Unbind: Bitcopy")
@@ -48,7 +35,7 @@ def main() -> None:
         count = data["COUNTS"][i]
         print(f"\t Iteration: {i + 1}")
         print(f"\t     Count: {count}")
-        print(f"\tProportion: {(count / S_COUNT) * 100:.2f}%")
+        print(f"\tProportion: {(count / SCAFFOLDS) * 100:.2f}%")
         print()
 
     print("\n")
